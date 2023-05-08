1aae9464b6334dff61a1b35c847b5327a4b7cdc6
Yanling Chen <cyl129@163.com>
2023-05-08 17:12:09 +0200

    Add merge profiles code

--- visualize/save_plots_indir_to_pdf.py
+++ visualize/save_plots_indir_to_pdf.py
@@ -2,27 +2,31 @@
 import os
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
+from glob import glob
 
+today = '2023-05-07'
 # set the path to the directory containing the plots
-plot_dir = "/Users/eusracenorth/Documents/suzaku_A222-223/xmm_data/merged/2023-05-04/png/2023-05-04"
+plot_dir = f"/Users/eusracenorth/Documents/suzaku_A222-223/xmm_data/merged/{today}_merge_obs_inst_bands/png/10am/{today}"
 
 # get a list of all files in the directory
-files = os.listdir(plot_dir)
+# files = os.listdir(plot_dir)
+files = glob(f'{plot_dir}/*inclfila_bigeschipgapmasked.png')
 # create a PDF object
-pdf = PdfPages(f"{plot_dir}/profiles_sep_instruments_bands_obs_2023-05-04.pdf")
+os.system(f'rm {plot_dir}/inclfila_bigeschipgapmasked_{today}.pdf')
+pdf = PdfPages(f"{plot_dir}/inclfila_bigeschipgapmasked_{today}.pdf")
 
 # loop over all files in the directory
 for file in files:
     # check if the file is a plot (i.e. has a ".png" extension)
-    if file.endswith(".png"):
-        # load the plot into memory
-        plot_dat = plt.imread(os.path.join(plot_dir, file))
-        # add the plot to the PDF object
-        plt.imshow(plot_dat)
-        plt.xticks([])
-        plt.yticks([])
-        pdf.savefig()
-        plt.close()
+    # if file.endswith(".png"):
+    # load the plot into memory
+    plot_dat = plt.imread(os.path.join(plot_dir, file))
+    # add the plot to the PDF object
+    plt.imshow(plot_dat)
+    plt.xticks([])
+    plt.yticks([])
+    pdf.savefig()
+    plt.close()
 
 # close the PDF object
 pdf.close()
